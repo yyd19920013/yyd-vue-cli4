9ca4ea1f127aff6f8f893b6f1601bb57ff20573b
yyd <609825025@qq.com>
2021-10-12 18:41:24 +0800

    翻书效果

--- src/views/demoBook/demoBook.vue
+++ src/views/demoBook/demoBook.vue
@@ -1,47 +1,52 @@
 <template>
     <div class="demoBook">
-        <div class="manual-wrap active">
-            <turn ref="turn" :width="width" :height="height" :data="dataList" @change="changeFn" @prev="prevFn" @next="nextFn" />
+        <div class="manual-wrap active" @touchmove.prevent v-show="show">
+            <flipPage ref="flipPage" :width="width" :height="height" :data="dataList" @change="changeFn" @prev="prevFn" @next="nextFn" />
         </div>
     </div>
 </template>
 <script>
-import turn from 'vue-flip-page';
+import flipPage from 'vue-flip-page';
 
 export default {
     data() {
         return {
-            width: 500,
-            height: 500,
-            dataList: [{ "picture_image": "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2600216442,2384386498&fm=15&gp=0.jpg", }, { "picture_image": "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2600216442,2384386498&fm=15&gp=0.jpg", }]
+            width: 375,
+            height: 667,
+            currentIndex: 0,
+            id: 0,
+            show: true,
+            dataList: [{ "picture_image": "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2600216442,2384386498&fm=15&gp=0.jpg", id: 1 }, { "picture_image": "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2600216442,2384386498&fm=15&gp=0.jpg", id: 2 }]
         }
     },
 
     mounted() {
-
+        setTimeout(() => {
+            this.handleSwitchManual(1);
+        }, 1000);
     },
 
     methods: {
         handleSwitchManual(index) {
             if (index === this.currentIndex) return;
-            this.$refs["turn"].toPage(index);
+            this.$refs["flipPage"].toPage(index);
             this.currentIndex = index;
-            this.goods_id = this.manuals[this.currentIndex].goods_id;
-            this.show = false;
+            this.id = this.dataList[this.currentIndex].id;
+            // this.show = false;
         },
-        changeFn(){
-
+        changeFn(index) {
+            console.log('changeFn', index);
         },
-        prevFn(){
-
+        prevFn(data) {
+            console.log('prevFn', data);
         },
-        nextFn(){
-
+        nextFn(data) {
+            console.log('nextFn', data);
         },
     },
 
     components: {
-        turn,
+        flipPage,
     },
 }
 </script>
@@ -53,7 +58,7 @@ export default {
         position: relative;
         z-index: 0;
         width: 100%;
-        height: 100%;
+        height: 100vh;
         opacity: 0;
         display: -webkit-box;
         display: -ms-flexbox;
@@ -73,12 +78,18 @@ export default {
         -ms-user-select: none;
         user-select: none;
         -o-user-select: none;
+        overflow: hidden;
     }
 
     .manual-wrap.active {
         -webkit-transform: scale(1);
         transform: scale(1);
         opacity: 1;
+    }
+
+    ::v-deep .page-count {
+        top: 10px !important;
+        padding: 0 10px !important;
     }
 }
 </style>
