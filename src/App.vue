044ea0be21c7e62e13e192d34e5357511898fc20
yyd <609825025@qq.com>
2021-10-27 19:51:51 +0800

    demo2

--- src/App.vue
+++ src/App.vue
@@ -51,6 +51,5 @@ export default {
 }
 </script>
 <style lang="scss">
-@import 'https://cdn.jsdelivr.net/npm/photo-sphere-viewer@4/dist/photo-sphere-viewer.min.css';
 @import '~css/index.scss';
 </style>
--- src/views/demo360/demo360.vue
+++ src/views/demo360/demo360.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="demo360">
+        <div id="container">
+        </div>
     </div>
 </template>
 <script>
 import { Viewer } from 'photo-sphere-viewer';
-import CubemapAdapter from 'photo-sphere-viewer/src/adapters/cubemap';
+import CubemapAdapter from 'photo-sphere-viewer/dist/adapters/cubemap';
+import MarkersPlugin from 'photo-sphere-viewer/dist/plugins/markers';
 
 export default {
     data() {
@@ -15,23 +18,109 @@ export default {
 
     mounted() {
         //一张全景图
-        // const viewer = new Viewer({
-        //     container: document.querySelector('.demo360'),
-        //     panorama: require('images/demo1.jpg'),
-        // });
+        const viewer = new Viewer({
+            container: document.querySelector('#container'),
+            panorama: require('images/demo2.jpg'),
+            plugins: [
+                [MarkersPlugin, {
+                    markers: [{
+                            id: 'marker1',
+                            longitude: '0deg',
+                            latitude: '0deg',
+                            image: require('images/dot.png'),
+                            width: 50,
+                            height: 50,
+                            style: {
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                cursor: 'point'
+                            },
+                            tooltip: {
+                                content: '三因科技有限公司',
+                                position: 'top center'
+                            },
+                        },
+                        {
+                            id: 'marker2',
+                            longitude: '-45deg',
+                            latitude: '0deg',
+                            image: require('images/dot.png'),
+                            width: 50,
+                            height: 50,
+                            style: {
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                cursor: 'point'
+                            },
+                            tooltip: {
+                                content: '杭州',
+                                position: 'top center'
+                            },
+                        },
+                        {
+                            id: 'marker3',
+                            longitude: '45deg',
+                            latitude: '0deg',
+                            image: require('images/dot.png'),
+                            width: 50,
+                            height: 50,
+                            style: {
+                                backgroundColor: 'rgba(0, 0, 0, 0)',
+                                cursor: 'point'
+                            },
+                            tooltip: {
+                                content: '欢迎您',
+                                position: 'top center'
+                            },
+                        }
+                    ],
+                }],
+            ],
+        });
 
         //六张全景图
-        const viewer = new Viewer({
-            container: document.querySelector('.demo360'),
-            adapter: CubemapAdapter,
-            panorama: {
-                left: require('images/six/left.jpg'),
-                front: require('images/six/front.jpg'),
-                right: require('images/six/right.jpg'),
-                back: require('images/six/back.jpg'),
-                top: require('images/six/top.jpg'),
-                bottom: require('images/six/bottom.jpg'),
-            },
+        // const viewer = new Viewer({
+        //     container: document.querySelector('#container'),
+        //     adapter: CubemapAdapter,
+        //     panorama: {
+        //         left: require('images/six/left.jpg'),
+        //         front: require('images/six/front.jpg'),
+        //         right: require('images/six/right.jpg'),
+        //         back: require('images/six/back.jpg'),
+        //         top: require('images/six/top.jpg'),
+        //         bottom: require('images/six/bottom.jpg'),
+        //     },
+        //     plugins: [
+        //         [MarkersPlugin, {
+        //             markers: [{
+        //                 id: 'marker1',
+        //                 longitude: '80deg',
+        //                 latitude: '-30deg',
+        //                 image: require('images/dot.png'),
+        //                 width: 50,
+        //                 height: 50,
+        //                 style: {
+        //                     backgroundColor: 'rgba(0, 0, 0, 0)',
+        //                     cursor: 'point'
+        //                 },
+        //                 tooltip: {
+        //                     content: '去这里？',
+        //                     position: 'top center'
+        //                 },
+        //             }],
+        //         }],
+        //     ],
+        // });
+
+        const markersPlugin = viewer.getPlugin(MarkersPlugin);
+
+        markersPlugin.on('select-marker', (e, marker) => {
+            console.log('选择点：', marker);
+            markersPlugin.updateMarker({
+                id: marker.id,
+                image: require('images/dot.png'),
+            });
+            markersPlugin.gotoMarker(marker.id, 1000).then(() => {
+
+            });
         });
     },
 
@@ -48,6 +137,15 @@ export default {
 @import '~css/public.scss';
 
 .demo360 {
-    height: 100vh;
+    #container {
+        height: 100vh;
+        #marker1 {
+            width: 20px;
+            height: 20px;
+            position: absolute;
+            left: 0;
+            top: 0;
+        }
+    }
 }
 </style>
