628418e66d6498c52e9753a3a52cbc46279f2b03
yyd <609825025@qq.com>
2021-10-28 11:32:09 +0800

    修改360demo

--- src/views/demo360/demo360.vue
+++ src/views/demo360/demo360.vue
@@ -2,6 +2,22 @@
     <div class="demo360">
         <div id="container">
         </div>
+        <audio :src="require('images/police_song.mp3')" preload="auto" loop="loop" ref="audio"></audio>
+        <el-dialog title="" :visible.sync="dialogVisible" width="30%">
+            <div class="wrap">
+                <div class="main">
+                    <img :src="require('images/logo.png')" alt=""/>
+                    <div class="text">
+                        <div>重庆三因兴云科技有限公司</div>
+                        <div>时刻为您服务！</div>
+                    </div>
+                </div>
+                <div class="end">
+                    <el-button type="primary" size="mini" icon="el-icon-video-play" @click="playMusic" v-if="isPlay"></el-button>
+                    <el-button type="warning" size="mini" icon="el-icon-video-pause" @click="pauseMusic" v-else-if="!isPlay"></el-button>
+                </div>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -12,7 +28,8 @@ import MarkersPlugin from 'photo-sphere-viewer/dist/plugins/markers';
 export default {
     data() {
         return {
-
+            dialogVisible: false,
+            isPlay:true,
         }
     },
 
@@ -24,54 +41,21 @@ export default {
             plugins: [
                 [MarkersPlugin, {
                     markers: [{
-                            id: 'marker1',
-                            longitude: '0deg',
-                            latitude: '0deg',
-                            image: require('images/dot.png'),
-                            width: 50,
-                            height: 50,
-                            style: {
-                                backgroundColor: 'rgba(0, 0, 0, 0)',
-                                cursor: 'point'
-                            },
-                            tooltip: {
-                                content: '三因科技有限公司',
-                                position: 'top center'
-                            },
+                        id: 'marker1',
+                        longitude: '0deg',
+                        latitude: '0deg',
+                        image: require('images/dot.png'),
+                        width: 50,
+                        height: 50,
+                        style: {
+                            backgroundColor: 'rgba(0, 0, 0, 0)',
+                            cursor: 'pointer'
                         },
-                        {
-                            id: 'marker2',
-                            longitude: '-45deg',
-                            latitude: '0deg',
-                            image: require('images/dot.png'),
-                            width: 50,
-                            height: 50,
-                            style: {
-                                backgroundColor: 'rgba(0, 0, 0, 0)',
-                                cursor: 'point'
-                            },
-                            tooltip: {
-                                content: '杭州',
-                                position: 'top center'
-                            },
-                        },
-                        {
-                            id: 'marker3',
-                            longitude: '45deg',
-                            latitude: '0deg',
-                            image: require('images/dot.png'),
-                            width: 50,
-                            height: 50,
-                            style: {
-                                backgroundColor: 'rgba(0, 0, 0, 0)',
-                                cursor: 'point'
-                            },
-                            tooltip: {
-                                content: '欢迎您',
-                                position: 'top center'
-                            },
-                        }
-                    ],
+                        // tooltip: {
+                        //     content: '重庆三因兴云 时刻为您服务',
+                        //     position: 'top center'
+                        // },
+                    }],
                 }],
             ],
         });
@@ -114,6 +98,7 @@ export default {
 
         markersPlugin.on('select-marker', (e, marker) => {
             console.log('选择点：', marker);
+            this.dialogVisible = true;
             markersPlugin.updateMarker({
                 id: marker.id,
                 image: require('images/dot.png'),
@@ -125,7 +110,14 @@ export default {
     },
 
     methods: {
-
+        playMusic() {
+            this.$refs.audio.play();
+            this.isPlay=false;
+        },
+        pauseMusic() {
+            this.$refs.audio.pause();
+            this.isPlay=true;
+        },
     },
 
     components: {
@@ -139,12 +131,36 @@ export default {
 .demo360 {
     #container {
         height: 100vh;
-        #marker1 {
-            width: 20px;
-            height: 20px;
-            position: absolute;
-            left: 0;
-            top: 0;
+    }
+    ::v-deep .el-dialog__body {
+        padding-top: 10px;
+        padding-bottom: 20px;
+    }
+    .wrap {
+        text-align: center;
+        .main{
+            padding: 20px 0;
+            img{
+                width: 100px;
+            }
+            .text{
+                padding-top: 10px;
+            }
+        }
+        .end {
+            display: flex;
+            align-items:end;
+            flex-direction:column;
+            .el-button{
+                display: flex;
+                justify-content:center;
+                width: 30px;
+                margin: 0;
+                margin-bottom: 5px;
+                span{
+                    font-size: 12px;
+                }
+            }
         }
     }
 }
